d0ae02d7d20d8954573c83831f85bcb1a4f5765c
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-07 20:53:43 +0500

    GroundChecker improvements Changed: Ground checker now may be configured to raycast selectively (and consider coins and/or enemies as ground or not)

--- Assets/Scripts/GroundChecker.cs
+++ Assets/Scripts/GroundChecker.cs
@@ -3,12 +3,34 @@
 public class GroundChecker : MonoBehaviour
 {
     public float GroundDistance;
+    public bool CoinsConsideredGround = false;
+    public bool EnemiesConsideredGround = true;
+
+    int _layerMask;
+
+    // Start is called just before any of the Update methods is called the first time
+    private void Start()
+    {
+        _layerMask = 1 << 11;//Platforms
+
+        if(CoinsConsideredGround)
+        {
+            int coinsLayer = 1 << 12;
+            _layerMask |= coinsLayer;
+        }
+
+        if(EnemiesConsideredGround)
+        {
+            int enemiesLayer = 1 << 13;
+            _layerMask |= enemiesLayer;
+        }
+    }
 
     public bool IsGrounded
     {
         get
         {
-            var hit = Physics2D.Raycast(this.transform.position, -this.transform.up, GroundDistance);
+            var hit = Physics2D.Raycast(this.transform.position, -this.transform.up, GroundDistance, _layerMask);
             return hit.collider != null;
         }
     }
