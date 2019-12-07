cad990d4bf2c43cceaee3c04433700cacd50d36d
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-07 12:40:31 +0500

    Added coin pickup system Added: CoinPicker script Added: TriggerProvider - script for acquiring trigger touches from another physical layer

---
+++ Assets/Scripts/CoinPicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinPicker : MonoBehaviour
+{
+    public TriggerProvider CoinToucher;
+    public Text CoinCounter;
+
+    int _coinCount;
+
+    // Start is called just before any of the Update methods is called the first time
+    private void Start()
+    {
+        CoinToucher.Triggered += HandleCoinTouch;
+        _coinCount = 0;
+        CoinCounter.text = "0";
+    }
+
+
+    void HandleCoinTouch(Collider2D collider)
+    {
+        string tag = collider.gameObject.tag;
+        Destroy(collider.gameObject);
+        _coinCount++;
+        CoinCounter.text = _coinCount.ToString();
+    }
+}
---
+++ Assets/Scripts/TriggerProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class TriggerProvider : MonoBehaviour
+{
+    public event Action<Collider2D> Triggered;
+
+    // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Triggered?.Invoke(collision);
+    }
+}
