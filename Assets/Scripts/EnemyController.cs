bad04b4e62747c51022bf1316493d9a871c96868
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-07 19:46:03 +0500

    EnemyController implemented

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,29 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(HitFromPlayerDetector))]
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyController : MonoBehaviour
 {
     public float Speed;
-    public float IdleTimeMin;
-    public float IdleTimeMax;
-    public GroundChecker[] GroundCheckers;
+    public float CycleEveryXSeconds;
+    public int MaxIdleCycles;
+    public GroundChecker LeftArm;
+    public GroundChecker RightArm;
+    public GroundChecker LeftLeg;
+    public GroundChecker RightLeg;
 
     bool _goingLeft;
+    int _idleXCycles;
+    int _idleCounter;
+    WaitForSeconds _interCycleWait;
     HitFromPlayerDetector _hitDetector;
+    Coroutine _cycleCoroutine;
+    Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         _goingLeft = Random.Range(0, 2) == 0 ? true : false;
 
+        _idleXCycles = Random.Range(1, MaxIdleCycles + 1);
+        _interCycleWait = new WaitForSeconds(CycleEveryXSeconds);
+
         _hitDetector = GetComponent<HitFromPlayerDetector>();
         _hitDetector.PlayerHitsFromAbove += HandleHitFromAbove;
         _hitDetector.PlayerHitsFromSide += HandlePlayerTouchFromSide;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _cycleCoroutine = StartCoroutine(RunMoveCycles());
     }
 
     void HandleHitFromAbove()
     {
+        StopCoroutine(_cycleCoroutine);
         GetComponent<Collider2D>().enabled = false;
     }
 
@@ -32,9 +50,38 @@ public class EnemyController : MonoBehaviour
         (GameObject.FindGameObjectWithTag("Player")?.GetComponent(typeof(PlayerDamager)) as PlayerDamager)?.HandleDamage();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RunMoveCycles()
     {
-        
+        while (true)
+        {
+            while(_idleCounter < _idleXCycles)
+            {
+                _idleCounter++;
+                yield return _interCycleWait;
+            }
+
+            _idleCounter = 0;
+            _idleXCycles = Random.Range(1, MaxIdleCycles + 1);
+
+            if(_goingLeft)
+            {
+                while(!LeftArm.IsGrounded && LeftLeg.IsGrounded)
+                {
+                    _rigidbody.velocity = Vector2.left * Speed;
+                    yield return _interCycleWait;
+                }
+            }
+            else
+            {
+                while (!RightArm.IsGrounded && RightLeg.IsGrounded)
+                {
+                    _rigidbody.velocity = Vector2.right * Speed;
+                    yield return _interCycleWait;
+                }
+            }
+
+            _goingLeft = !_goingLeft;//Swap direction
+            _rigidbody.velocity = Vector2.zero;
+        }
     }
 }
